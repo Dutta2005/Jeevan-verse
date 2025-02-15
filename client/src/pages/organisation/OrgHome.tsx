cf7871347226dd6e6e7b30724acaeb09db2125cd
soham247 <sohamsadhukhan247@gmail.com>
2025-02-15 07:46:36 +0530

    Org dashboard

--- client/src/pages/organisation/OrgHome.tsx
+++ client/src/pages/organisation/OrgHome.tsx
@@ -1,24 +1,23 @@
-import OrgLogoutBtn from "@/components/organisation/OrgLogoutBtn";
-import { Button } from "@/components/ui/button";
+import Header from "@/components/organisation/dashboard/Header";
+import PastActivities from "@/components/organisation/dashboard/PastActivities";
+import OrganizationLandingPage from "@/components/organisation/OrgLandingPage";
 import { RootState } from "@/store/store";
-import { Plus } from "lucide-react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router";
 
 function OrgHome() {
     const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated)
     return (
         <div>
-            <h1>Organisation Home</h1>
-            <Link to="create">
-                <Button className="bg-primary hover:bg-primary/90 text-white">
-                    <Plus className="mr-2 h-5 w-5" />
-                    Create Post
-                </Button>
-            </Link>
-            {isAuthenticated ? <OrgLogoutBtn /> : <p>You are not logged in</p>}
+            {isAuthenticated ? (
+            <div className="py-7">
+                <Header />
+                <PastActivities />
+            </div>
+            ) : (
+                <OrganizationLandingPage />
+            )}
         </div>
-    );
+    )
 }
 
 export default OrgHome
